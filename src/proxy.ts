672f609ef87319c7dd4b7249cb94916723ae347a
henhernquist-lab <henhernquist@gmail.com>
2026-07-01 19:22:02 +0000

    upgrade next-auth to v5

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,8 @@
 import { auth } from '@/lib/auth'
 import { NextResponse } from 'next/server'
-import type { NextRequestWithAuth } from 'next-auth/middleware'
 
-export default auth((req: NextRequestWithAuth) => {
-  if (!req.nextauth.token) {
+export default auth((req) => {
+  if (!req.auth) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
 })
